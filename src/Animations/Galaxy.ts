a457c7061e6a44b990dc446b2cc489e752f456f6
elShailee <shailee.eliyahu@gmail.com>
2023-10-29 22:29:15 +0200

    finished implementation

--- src/Animations/Galaxy.ts
+++ src/Animations/Galaxy.ts
@@ -1,25 +1,34 @@
 import AnimationCanvas from '../AnimationCanvas';
-import { getLineaRainbowColor, getRadialRainbowColor } from '../Utils/colors';
+import { getGradientRainbowColor, getLineaRainbowColor, getRadialRainbowColor } from '../Utils/colors';
 import { Position, drawCircle } from '../Utils/draw';
 
 const { abs, sin, cos } = Math;
+
+const numOfCirclesMultiplier = 15000;
 
 const coloringFuncs = [
 	() => 'rgba(0, 0, 0, 0.5)',
 	(props: { position: Position; canvas: AnimationCanvas }) => getRadialRainbowColor(props),
 	(props: { position: Position; canvas: AnimationCanvas }) => getLineaRainbowColor(props),
+	(props: { val: number; scale: number; canvas: AnimationCanvas }) => getGradientRainbowColor(props),
 ];
 
-export function drawNewAnimation(canvas: AnimationCanvas) {
-	canvas._ctx.strokeStyle = 'rgba(0, 0, 0, 0.5)';
+export function drawGalaxy(canvas: AnimationCanvas) {
+	const numOfCircles = numOfCirclesMultiplier / canvas._cellSize;
 	const time = canvas._animationTime * 0.000001;
-	for (let i = 0; i < 1000; i++) {
+
+	for (let i = 0; i < numOfCircles; i++) {
 		const speed = time * (i + 1);
 		const pos: Position = {
 			x: canvas._width * 0.5 + canvas._width * (sin(speed) * 100 + cos(speed * 0.5) * 25 - sin(speed * 0.3) * 20) * 0.003,
 			y: canvas._height * 0.54 + canvas._width * (cos(speed) * 100 + sin(speed * 0.5) * 25 - cos(speed * 0.3) * 20) * 0.0015,
 		};
-		canvas._ctx.strokeStyle = coloringFuncs[canvas._colorsCounter % coloringFuncs.length]({ position: pos, canvas });
+		canvas._ctx.strokeStyle = coloringFuncs[canvas._colorsCounter % coloringFuncs.length]({
+			val: i,
+			scale: numOfCircles,
+			position: pos,
+			canvas,
+		});
 		drawCircle(canvas._ctx, pos, abs(sin(speed * 1.4) * 10 + cos(speed * 0.5) * 10 + sin(speed * 0.2) * 10));
 	}
 }
--- src/Utils/colors.ts
+++ src/Utils/colors.ts
@@ -28,3 +28,12 @@ export const getLineaRainbowColor = ({ position, canvas }: Props) => {
 	const hue = gradient * completeRainbowInScreen + canvas._animationTime * cycleSpeed;
 	return `hsl(${hue}, 90%, 50%)`;
 };
+
+export const getGradientRainbowColor = ({ val, scale, canvas }: { val: number; scale: number; canvas: AnimationCanvas }) => {
+	const cycleSpeed = 0.05;
+	const completeRainbowInScreen = 2;
+	const gradient = (360 * val) / scale;
+
+	const hue = gradient * completeRainbowInScreen + canvas._animationTime * cycleSpeed;
+	return `hsl(${hue}, 90%, 50%)`;
+};
