662f71b83a7b1b8d4b64aa12e7fcd2e8543af8d6
elShailee <shailee.eliyahu@gmail.com>
2023-11-04 15:21:30 +0200

    some progress regarding graphing

--- src/Animations/MathGraph/MathGraph.ts
+++ src/Animations/MathGraph/MathGraph.ts
@@ -1,5 +1,5 @@
 import AnimationCanvas from '../../AnimationCanvas';
-import { drawCircleFill } from '../../Utils/draw';
+import { drawCircleFill, drawLine } from '../../Utils/draw';
 import { drawGridLines } from './drawGridLines';
 import { drawGridNumbers } from './drawGridNumbers';
 
@@ -43,4 +43,43 @@ export function drawMathGraph(canvas: AnimationCanvas) {
 	const y = centerY + distanceFromOrigin * sin(angle);
 	const radius = cellSize * 0.1;
 	drawCircleFill(ctx, { x, y }, radius);
+
+	const func = (x: number) => Math.log2(x);
+
+	for (let x = 0; x <= centerX * Math.min(1, animationTime / 100000); x += 0.001) {
+		drawLine(
+			ctx,
+			{
+				x: centerX + x * cellSize * cellSize,
+				y: centerY - func(x * cellSize) * cellSize,
+			},
+			{
+				x: centerX + (x + 0.001) * cellSize * cellSize,
+				y: centerY - func((x + 0.001) * cellSize) * cellSize,
+			},
+		);
+		drawLine(
+			ctx,
+			{
+				x: centerX - x * cellSize * cellSize,
+				y: centerY - func(-x * cellSize) * cellSize,
+			},
+			{
+				x: centerX - (x + 0.001) * cellSize * cellSize,
+				y: centerY - func(-(x + 0.001) * cellSize) * cellSize,
+			},
+		);
+	}
+
+	// // drawCircleFill(ctx, { x: centerX, y: 100 + centerY }, radius);
+	// // drawCircleFill(ctx, { x: 180 + centerX, y: 100 + centerY }, radius);
+	// // drawCircleFill(ctx, { x: 180 + centerX, y: centerY }, radius);
+	// // drawCircleFill(ctx, { x: 180 + centerX, y: -100 + centerY }, radius);
+	// // drawCircleFill(ctx, { x: 380 + centerX, y: -100 + centerY }, radius);
+	// // drawCircleFill(ctx, { x: 380 + centerX, y: centerY }, radius);
+	// ctx.beginPath();
+	// ctx.moveTo(0 + centerX, centerY);
+	// ctx.bezierCurveTo(0 + centerX, 100 + centerY, 180 + centerX, 100 + centerY, 180 + centerX, 0 + centerY);
+	// ctx.bezierCurveTo(180 + centerX, -100 + centerY, 380 + centerX, -100 + centerY, 380 + centerX, 0 + centerY);
+	// ctx.stroke();
 }
